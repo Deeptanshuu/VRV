43ae4c655fee5e0bf3ce22135304dbc168516dc8
Deeptanshu Lal <laldeeptanshu96@gmail.com>
2025-01-07 23:14:38 +0530

    time waste

--- Code/vite.config.js
+++ Code/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ReactComponentName from "react-scan/react-component-name/vite"; 
 
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
-    ReactComponentName({}), 
   ],
   build: {
     rollupOptions: {
